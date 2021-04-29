a366d4593553c44d4dcdd52538743009f44f1819
MaySoMusician <maysomusician@gmail.com>
2021-04-30 04:02:54 +0900

    Add AppSpacer component ported from ChakraUI

---
+++ components/AppSpacer.vue
@@ -0,0 +1,11 @@
+<template>
+  <CBox flex="1 1 0%" place-self="stretch" />
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend<unknown, unknown, unknown, unknown>({})
+</script>
+
+<style></style>
