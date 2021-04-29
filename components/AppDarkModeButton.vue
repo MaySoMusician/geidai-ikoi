9c505ed5804464252022405af6d3c94ea56e0cd4
MaySoMusician <maysomusician@gmail.com>
2021-04-30 04:03:26 +0900

    Split AppDarkModeButton from sample index page

---
+++ components/AppDarkModeButton.vue
@@ -0,0 +1,29 @@
+<template>
+  <CIconButton
+    mr="3"
+    :icon="colorMode === 'light' ? 'moon' : 'sun'"
+    :aria-label="`Switch to ${colorMode === 'light' ? 'dark' : 'light'} mode`"
+    @click="$emit('click')"
+  />
+</template>
+
+<script lang="ts">
+import Vue, { PropType } from 'vue'
+
+export type ColorMode = 'light' | 'dark'
+
+type Props = {
+  colorMode: ColorMode
+}
+
+export default Vue.extend<unknown, unknown, unknown, Props>({
+  props: {
+    colorMode: {
+      type: String as PropType<ColorMode>,
+      required: true,
+    },
+  },
+})
+</script>
+
+<style></style>
