b68b0827c4989c6b1bd0393ce66fd9aadf4bbcf3
MaySoMusician <maysomusician@gmail.com>
2021-05-02 05:53:21 +0900

    Add Notion-as-a-CMS wrapper

---
+++ utils/notion.ts
@@ -0,0 +1,56 @@
+const NOTION_API_BASEURL = 'https://notion-api.splitbee.io/v1'
+const IGNORE = true as const
+
+export function fetchNotionData(type: string, id: string) {
+  return fetch(`${NOTION_API_BASEURL}/${type}/${id}`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'text/plain',
+    },
+  })
+}
+
+export function fetchNotionTableData(id: string) {
+  return fetchNotionData('table', id)
+}
+
+export interface NotionDatabaseItem {
+  id: string
+}
+
+export function isValidNotionDatabaseItem(
+  target: any
+): target is NotionDatabaseItem {
+  return _isValidNotionDatabaseItem(target)
+}
+
+function _isValidNotionDatabaseItem(target: any): boolean {
+  return target && isNotEmptyString(target.id)
+}
+
+export interface MeetLinkItem extends NotionDatabaseItem {
+  slug: string
+  url: string
+  category?: string[]
+  name?: string
+  isHidden?: boolean
+}
+
+export function isValidMeetLinkItem(target: any): target is MeetLinkItem {
+  return (
+    _isValidNotionDatabaseItem(target) &&
+    isNotEmptyString(target.slug) &&
+    isNotEmptyString(target.url) &&
+    target.url.startsWith('https://meet.google.com/') &&
+    ('category' in target
+      ? Array.isArray(target.category) &&
+        target.category.every((v: any) => isNotEmptyString(v))
+      : IGNORE) &&
+    ('name' in target ? isNotEmptyString(target.name) : IGNORE) &&
+    ('isHidden' in target ? typeof target.isHidden === 'boolean' : IGNORE)
+  )
+}
+
+function isNotEmptyString(target: any): target is string {
+  return typeof target === 'string' && target !== ''
+}
