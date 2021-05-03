e8042905d2a4ab05b6b3f7727e837237813c9877
MaySoMusician <maysomusician@gmail.com>
2021-05-03 19:27:42 +0900

    Update website title & description

--- utils/constants.ts
+++ utils/constants.ts
@@ -2,5 +2,5 @@ export const FORBIDDEN_OUTSIDE_OF_UNIV = 'FORBIDDEN_OUTSIDE_OF_UNIV'
 
 export const QUERY_ERROR_SIGNIN_REQUIRED = '1'
 
-export const WEBSITE_NAME = '芸大 オンライン憩い ☕'
-export const WEBSITE_DESCRIPTION = ''
+export const WEBSITE_NAME = '藝大 オンライン憩い ☕'
+export const WEBSITE_DESCRIPTION = '「憩い」と呼ばれる場所がある'
