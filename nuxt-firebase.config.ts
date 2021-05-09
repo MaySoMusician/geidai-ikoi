50c695204d7b2175428a9e5265bd321dea967d4d
MaySoMusician <maysomusician@gmail.com>
2021-05-09 16:35:50 +0900

    Disable Firebase analytics

--- nuxt-firebase.config.ts
+++ nuxt-firebase.config.ts
@@ -9,7 +9,7 @@ const config: FirebaseModuleConfiguration = {
     storageBucket: 'geidai-ikoi-01.appspot.com',
     messagingSenderId: '898931287801',
     appId: '1:898931287801:web:4bef235eb465f887c22428',
-    measurementId: 'G-LM2Q0T0TK9',
+    measurementId: '',
   },
   services: {
     auth: {
@@ -20,7 +20,7 @@ const config: FirebaseModuleConfiguration = {
           mutationNames.ON_AUTH_STATE_CHANGED_MUTATION,
       },
     },
-    analytics: true,
+    analytics: false,
   },
   onFirebaseHosting: true,
 }
