28f3e246b29f9ae91847b1ffcc6dc9f430989ea4
MaySoMusician <maysomusician@gmail.com>
2021-06-07 00:11:02 +0900

    Change TheWebsiteTitle design

--- components/TheWebsiteTitle.vue
+++ components/TheWebsiteTitle.vue
@@ -1,9 +1,32 @@
 <template>
-  <CHeading as="h1" text-align="center" :mt="3" :mb="4" :class="[$style.Title]">
-    <CLink as="nuxt-link" to="/" :_hover="{ textDecoration: 'none' }">
-      <AppSvgIconCoffee /><span>オンライン憩い</span><AppSvgIconCoffee />
-    </CLink>
-  </CHeading>
+  <CBox>
+    <CBox :class="[$style.Header]">
+      <CFlex :class="[$style.HeaderLogoContainer]" direction="row">
+        <CLink as="nuxt-link" d="block" to="/" :_focus="{}">
+          <img
+            :class="[$style.HeaderLogoImage]"
+            src="https://via.placeholder.com/96"
+          />
+        </CLink>
+      </CFlex>
+      <CFlex :class="[$style.HeaderLinksOuter]" direction="row" ml="">
+        <CFlex
+          :class="[$style.HeaderLinks]"
+          direction="row"
+          justify="center"
+          text-align="center"
+          font-size="0.8rem"
+        >
+          <CBox :pt="1" :pb="2">憩いとは</CBox>
+          <CBox :pt="1" :pb="2">運営より</CBox>
+          <CBox :pt="1" :pb="2">募集案内</CBox>
+        </CFlex>
+      </CFlex>
+    </CBox>
+    <CBox :class="[$style.AboveTheFold]">
+      <img :class="[$style.AboveTheFoldBackground]" src="/photo-ikoi06.jpg" />
+    </CBox>
+  </CBox>
 </template>
 
 <script lang="ts">
@@ -12,16 +35,72 @@ export default Vue.extend<unknown, unknown, unknown, unknown>({})
 </script>
 
 <style lang="scss" module>
-.Title span {
-  vertical-align: middle;
-  margin: {
-    left: 6px;
-    right: 6px;
-  }
-  font-size: 1.65rem;
+.Header {
+  $bg: rgba(255, 255, 255, 88%);
+  position: relative;
 
-  @media screen and (min-width: 30em) {
-    font-size: 1.875rem;
+  &Logo {
+    &Container {
+      position: absolute;
+      background: $bg;
+      z-index: 1;
+
+      margin: {
+        left: 1.3rem;
+      }
+    }
+
+    &Image {
+      padding: 0.1rem 0.3rem;
+      padding: {
+        top: 0.1rem;
+        bottom: 0.2rem;
+        left: 0.2rem;
+        right: 0.2rem;
+      }
+    }
+  }
+
+  &Links {
+    padding: {
+      top: 0.3rem;
+      bottom: 0.3rem;
+    }
+
+    &Outer {
+      position: absolute;
+      background: $bg;
+      margin: {
+        left: calc(1.3rem + 96px + 0.4rem);
+      }
+      z-index: 1;
+    }
+
+    > div {
+      margin: 0 1rem;
+    }
+  }
+}
+
+.AboveTheFold {
+  position: relative;
+  overflow: hidden;
+
+  padding: {
+    bottom: 12rem;
+  }
+
+  margin: {
+    bottom: 1.3rem;
+  }
+
+  &Background {
+    position: absolute;
+    height: 100%;
+    width: 100%;
+    object-fit: cover;
+    object-position: 50% 66%;
+    z-index: 0;
   }
 }
 </style>
