3fa781487c2fad0d67aeea730a7a67d25a049ecd
MaySoMusician <maysomusician@gmail.com>
2021-06-06 21:30:05 +0900

    Register brand colors

--- nuxt-chakra-ui.config.ts
+++ nuxt-chakra-ui.config.ts
@@ -24,6 +24,15 @@ export const extendTheme: Partial<ChakraTheme> = {
     'dark-lg':
       'rgba(0, 0, 0, 0.1) 0px 0px 0px 1px, rgba(0, 0, 0, 0.2) 0px 5px 10px, rgba(0, 0, 0, 0.4) 0px 15px 40px',
   },
+  colors: {
+    grass: {
+      50: '#dbdad3',
+      400: '#9e9d89',
+    },
+    wafer: '#e4d3cf',
+    cavernPink: '#e2bcb7',
+    santafe: '#b67162',
+  },
 }
 
 type Icon = {
