c6c478cd7d6650920abb75971723c30a56d7550f
MaySoMusician <maysomusician@gmail.com>
2021-07-31 00:06:55 +0900

    Change layout of TheNewsList, add padding, change when 'loaded' event is fired

--- components/TheNewsList.vue
+++ components/TheNewsList.vue
@@ -1,38 +1,51 @@
 <template>
-  <transition name="fade" mode="out-in" @enter="$emit('loaded')">
-    <CFlex v-if="pending" v-bind="$attrs" key="loading" justify="center">
-      <AppSpinnerLoading />
-    </CFlex>
-    <CFlex v-else-if="error" key="error" justify="center" v-bind="$attrs">
-      <AppErrorLoading />
-    </CFlex>
-    <template v-else>
-      <CAlert
-        v-if="newsItemsAvailable.length < 1"
-        v-bind="$attrs"
-        key="shown"
-        :class="[$style.News]"
-        >お知らせはありません</CAlert
-      >
-      <CStack v-else v-bind="$attrs" key="shown" :spacing="3">
-        <CAlert
-          v-for="(item, index) in newsItemsAvailable"
-          :key="index"
-          :status="item.type"
-        >
-          <CAlertIcon />
-          <CBox>
-            <CAlertTitle :mr="2">{{ item.title }}</CAlertTitle>
-            <CAlertDescription v-if="item.content">{{
-              item.content
-            }}</CAlertDescription>
-          </CBox>
-        </CAlert>
-        <!-- Avoid empty slot -->
-        <div />
-      </CStack>
-    </template>
-  </transition>
+  <CBox :class="[$style.NewsWrapper]">
+    <client-only>
+      <transition name="fade" mode="out-in">
+        <CFlex v-if="pending" v-bind="$attrs" key="loading" justify="center">
+          <AppSpinnerLoading color="white" size="2rem" />
+        </CFlex>
+        <CFlex v-else-if="error" key="error" justify="center" v-bind="$attrs">
+          <AppErrorLoading />
+        </CFlex>
+        <template v-else>
+          <CAlert
+            v-if="newsItemsAvailable.length < 1"
+            v-bind="$attrs"
+            key="shown"
+            :class="[$style.NewsItem]"
+            bg="transparent"
+            >お知らせはありません</CAlert
+          >
+          <CStack v-else v-bind="$attrs" key="shown" :spacing="0">
+            <CAlert
+              v-for="(item, index) in newsItemsAvailable"
+              :key="index"
+              :class="[$style.NewsItem]"
+              :status="item.type"
+              bg="transparent"
+              :py="3"
+            >
+              <CAlertIcon />
+              <CBox>
+                <CAlertTitle d="inline" :mr="2">{{ item.title }}</CAlertTitle>
+                <CAlertDescription d="inline">
+                  <CText v-if="item.content" as="span">{{
+                    item.content
+                  }}</CText>
+                  <time v-if="item.publishedOn" :datetime="item.publishedOn"
+                    >（{{ item.publishedOn }}）</time
+                  >
+                </CAlertDescription>
+              </CBox>
+            </CAlert>
+            <!-- Avoid empty slot -->
+            <div />
+          </CStack>
+        </template>
+      </transition>
+    </client-only>
+  </CBox>
 </template>
 
 <script lang="ts">
@@ -75,6 +88,7 @@ export default Vue.extend<Data, unknown, Computed, unknown>({
       this.error = e
     } finally {
       this.pending = false
+      this.$emit('loaded')
     }
   },
 })
@@ -86,14 +100,20 @@ export default Vue.extend<Data, unknown, Computed, unknown>({
 
 <style lang="scss" module>
 .News {
-  padding: {
-    bottom: 1.6rem;
+  &Wrapper {
+    padding: {
+      top: 0.4rem;
+      bottom: 1.2rem;
+    }
+    background: linear-gradient(
+      to top,
+      rgb(255, 255, 255),
+      var(--theme-colors-ebb) 1.2rem
+    );
   }
-  background: linear-gradient(
-    to bottom,
-    var(--theme-colors-ebb),
-    var(--theme-colors-ebb) 55%,
-    rgb(255, 255, 255)
-  );
+  &Item {
+    letter-spacing: 0.4px;
+    font-size: 0.92rem;
+  }
 }
 </style>
