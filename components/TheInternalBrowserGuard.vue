65511dc2399b29557325aa02405bcfe5c30c5626
MaySoMusician <maysomusician@gmail.com>
2021-10-18 09:18:07 +0900

    Remove test code

--- components/TheInternalBrowserGuard.vue
+++ components/TheInternalBrowserGuard.vue
@@ -1,22 +1,6 @@
 <template>
   <CBox :mb="1">
     <CText font-size="0.8rem">{{ userAgent }} </CText>
-
-    <textarea
-      ref="logsTextArea"
-      :value="getNavigator()"
-      readonly
-      rows="3"
-      style="
-        width: 100%;
-        font-family: monospace;
-        font-size: 11px;
-        line-height: 110%;
-        padding: 3px;
-      "
-      @click="() => $refs.logsTextArea && $refs.logsTextArea.select()"
-    >
-    </textarea>
   </CBox>
 </template>
 
@@ -29,9 +13,7 @@ type Computed = {
   userAgent: string
 }
 
-type Methods = {
-  getNavigator(): string
-}
+type Methods = {}
 
 export default Vue.extend<Data, unknown, Computed, Methods>({
   components: {},
@@ -44,35 +26,7 @@ export default Vue.extend<Data, unknown, Computed, Methods>({
     },
   },
   mounted() {},
-  methods: {
-    getNavigator() {
-      if (!process.client) return ''
-
-      const getObject = function (object: any, space: number) {
-        let r = ''
-        for (const key in object) {
-          const s = ' '.repeat(space)
-          if (['plugins', 'enabledPlugin'].includes(key)) {
-            r += `${s}${key} : *omitted*\n`
-            continue
-          }
-
-          const cur = object[key]
-
-          if (typeof cur === 'function') {
-            r += `${s}${key} : function\n`
-          } else if (typeof cur === 'object') {
-            r += `${s}${key} : \n${getObject(cur, space + 2)}\n`
-          } else {
-            r += `${s}${key} : "${cur}" [${typeof cur}]\n`
-          }
-        }
-        return r
-      }
-
-      return getObject(navigator, 0)
-    },
-  },
+  methods: {},
 })
 </script>
 
