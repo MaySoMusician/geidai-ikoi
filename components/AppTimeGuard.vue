063d6bc4436eff60c764b15bdcb4ad502fabe81c
MaySoMusician <maysomusician@gmail.com>
2021-11-01 00:29:23 +0900

    Make time guard properly calculate current time

--- components/AppTimeGuard.vue
+++ components/AppTimeGuard.vue
@@ -7,6 +7,10 @@
 <script lang="ts">
 import Vue from 'vue'
 
+type Data = {
+  now: number | undefined
+}
+
 type Props = {
   start: Date | undefined
   end: Date | undefined
@@ -18,7 +22,7 @@ type Computed = {
   afterStart: boolean
 }
 
-export default Vue.extend<unknown, unknown, Computed, Props>({
+export default Vue.extend<Data, unknown, Computed, Props>({
   props: {
     start: {
       type: Date,
@@ -29,16 +33,24 @@ export default Vue.extend<unknown, unknown, Computed, Props>({
       default: undefined,
     },
   },
+  data() {
+    return {
+      now: undefined,
+    }
+  },
   computed: {
     isShown() {
       return this.beforeEnd && this.afterStart
     },
     beforeEnd() {
-      return this.end ? Date.now() < this.end.getTime() : true
+      return this.now && this.end ? Date.now() < this.end.getTime() : true
     },
     afterStart() {
-      return this.start ? Date.now() >= this.start.getTime() : true
+      return this.now && this.start ? Date.now() >= this.start.getTime() : true
     },
+  },
+  mounted() {
+    this.now = Date.now()
   },
 })
 </script>
